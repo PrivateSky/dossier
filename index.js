c8f577cae4f933d661570c7e33bdeff7f9ff9866
asac <asac.cool@gmail.com>
2020-02-19 15:25:38 +0200

    api refactor

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-module.exports.load = function(seed, callback){
+module.exports.load = function(seed, identity, callback){
 //        const edfs = require("edfs").attachFromSeed(seed);
     const se = require("swarm-engine");
     se.initialise();
@@ -7,18 +7,18 @@ module.exports.load = function(seed, callback){
     const pathName = "path";
     const path = require(pathName);
     const powerCord = se.OuterThreadPowerCord(path.join(process.env.PSK_ROOT_INSTALATION_FOLDER, "psknode/bundles/threadBoot.js"), false, seed);
-    $$.swarmEngine.plug("*", powerCord);
+    $$.swarmEngine.plug("identityName", powerCord);
 
     const handler = {
         attachTo : $$.interactions.attachTo,
         startTransaction : function (swarmTypeName, phaseName, ...args) {
             //todo: get identity from context somehow
-            return $$.interactions.startSwarmAs("anonymous", swarmTypeName, phaseName, ...args);
+            return $$.interactions.startSwarmAs("identityName", swarmTypeName, phaseName, ...args);
         }
     };
     //return handler;
     //todo implement a way to know when thread is ready
     setTimeout(()=>{
         callback(undefined, handler);
-    }, 1000);
+    }, 100);
 };
