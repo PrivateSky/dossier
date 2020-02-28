43c5ec3baa302976a3a14940d62615a8f3634e17
asaccool <asac.cool@gmail.com>
2020-02-28 14:47:31 +0200

    load api can be called multiple times for different seeds

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
+const se = require("swarm-engine");
+se.initialise();
 
 module.exports.load = function(seed, identity, callback){
-//        const edfs = require("edfs").attachFromSeed(seed);
-    const se = require("swarm-engine");
-    se.initialise();
-    //TODO: maybe we should get the boot script from the same csb? :-?
     const pathName = "path";
     const path = require(pathName);
     const powerCord = se.OuterThreadPowerCord(path.join(process.env.PSK_ROOT_INSTALATION_FOLDER, "psknode/bundles/threadBoot.js"), false, seed);
-    $$.swarmEngine.plug("identityName", powerCord);
+
+    let cord_identity;
+    try{
+        const crypto = require("pskcrypto");
+        cord_identity = crypto.pskHash(seed, "hex");
+        $$.swarmEngine.plug(cord_identity, powerCord);
+    }catch(err){
+        return callback(err);
+    }
 
     const handler = {
         attachTo : $$.interactions.attachTo,
         startTransaction : function (transactionTypeName, methodName, ...args) {
             //todo: get identity from context somehow
-            return $$.interactions.startSwarmAs("identityName", "transactionHandler", "start", identity, transactionTypeName, methodName, ...args);
+            return $$.interactions.startSwarmAs(cord_identity, "transactionHandler", "start", identity, transactionTypeName, methodName, ...args);
         }
     };
-    //return handler;
     //todo implement a way to know when thread is ready
     setTimeout(()=>{
         callback(undefined, handler);
