7ebd9ef6cc2985608bee5097b656d8de0fe27b60
Nicu-Cosmin Ursache <asac.cool@gmail.com>
2020-05-20 14:46:23 +0300

    web workers wip

--- index.js
+++ index.js
@@ -3,11 +3,7 @@ if(typeof $$ === "undefined" || typeof $$.swarmEngine === "undefined"){
     se.initialise();
 }
 
-module.exports.load = function(seed, identity, callback){
-    const pathName = "path";
-    const path = require(pathName);
-    const powerCord = new se.OuterThreadPowerCord(path.join(process.env.PSK_ROOT_INSTALATION_FOLDER, "psknode/bundles/threadBoot.js"), false, seed);
-
+function envSetup(powerCord, seed, identity, callback){
     let cord_identity;
     try{
         const crypto = require("pskcrypto");
@@ -28,9 +24,30 @@ module.exports.load = function(seed, identity, callback){
                 return $$.interactions.startSwarmAs(cord_identity, "transactionHandler", "start", identity, transactionTypeName, methodName, ...args);
             }
         };
-        //todo implement a way to know when thread is ready
+        //todo implement a way to know when thread/worker/isolate is ready
         setTimeout(()=>{
             callback(undefined, handler);
         }, 100);
     });
-};
+}
+
+module.exports.load = function(seed, identity, callback){
+    const envTypes = require("overwrite-require").constants;
+    switch($$.environmentType){
+        case envTypes.BROWSER_ENVIRONMENT_TYPE:
+            const pc = new se.OuterWebWorkerPowerCord("path_to_boot_script", seed);
+            return envSetup(pc, seed, identity, callback);
+            break;
+        case envTypes.NODEJS_ENVIRONMENT_TYPE:
+            const pathName = "path";
+            const path = require(pathName);
+            const powerCord = new se.OuterThreadPowerCord(path.join(process.env.PSK_ROOT_INSTALATION_FOLDER, "psknode/bundles/threadBoot.js"), false, seed);
+            return envSetup(powerCord, seed, identity, callback);
+            break;
+        case envTypes.SERVICE_WORKER_ENVIRONMENT_TYPE:
+        case envTypes.ISOLATE_ENVIRONMENT_TYPE:
+        case envTypes.THREAD_ENVIRONMENT_TYPE:
+        default:
+            return callback(new Error(`Dossier can not be loaded in <${$$.environmentType}> environment type for now!`));
+    }
+}
